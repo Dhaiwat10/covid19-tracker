b3c7e46cef561e44e55bd338989cb034a51d13db
Dhaiwat10 <dhaiwatpandya@gmail.com>
2020-03-17 16:25:28 +0530

    Updates

---
+++ component---src-pages-404-js-c0dc9cd6c4e69aa6e5d2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"404 - Page not found.")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c0dc9cd6c4e69aa6e5d2.js.map
